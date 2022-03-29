f00f4fbdff61178235703a13508544185c8811c2
Oncu <oohancan@gmail.com>
2022-03-29 10:57:09 +0300

    add main and day view

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
                 .font(.system(size: 32, weight: .medium, design: .default))
                 .foregroundColor(.white)
                 .padding()
-                VStack {
+                VStack(spacing: 10) {
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
@@ -27,14 +27,50 @@ struct ContentView: View {
                         .frame(width: 180, height: 180)
                     Text("76°")
                         .font(.system(size: 70, weight: .medium))
-                }
+                        .foregroundColor(.white)
+                    .foregroundColor(.white)
+            }
+                .padding(.bottom, 40)
+                HStack (spacing: 20){
+                WeatherDayView(dayOfWeek: "TUE", imageName: "cloud.sun.fill", temperature: 75)
+                WeatherDayView(dayOfWeek: "WED", imageName: "sun.max.fill", temperature: 88)
+                WeatherDayView(dayOfWeek: "THU", imageName: "wind.snow", temperature: 55)
+                WeatherDayView(dayOfWeek: "FRI", imageName: "sunset.fill", temperature: 60)
+                WeatherDayView(dayOfWeek: "SAT", imageName: "snow",
+                    temperature: 26)
+            }
             Spacer()
-        }
 }
+    }
     }
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 20, weight: .medium))
+                .foregroundColor(.white)
+                .foregroundColor(.white)
+        }
     }
 }
